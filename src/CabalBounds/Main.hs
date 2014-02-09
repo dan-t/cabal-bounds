5285bfefa32fb0d91009c1d3cd39c28af26d77bb
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-02-09 16:38:32 +0100

    Add function 'cabalBounds' for easier testing

---
+++ src/CabalBounds/Main.hs
@@ -0,0 +1,26 @@
+
+module CabalBounds.Main
+   ( cabalBounds
+   ) where
+
+import Distribution.PackageDescription.Parse (parsePackageDescription, ParseResult(..))
+import Distribution.PackageDescription.PrettyPrint (showGenericPackageDescription)
+import qualified CabalBounds.Args as Args
+import CabalBounds.Command (command)
+import CabalBounds.Execute (execute)
+import qualified System.IO.Strict as SIO
+import System.Exit (exitFailure, exitSuccess)
+import System.IO (hPutStrLn, stderr)
+
+cabalBounds :: Args.Args -> IO ()
+cabalBounds args  = do
+   file <- SIO.readFile (Args.cabalFile args)
+   case parsePackageDescription file of
+        ParseFailed error   -> hPutStrLn stderr ("cabal-bounds: " ++ show error) >> exitFailure
+        ParseOk _ pkgDescrp -> do
+           let pkgDescrp' = execute (command args) pkgDescrp
+               outFile | null $ Args.outputCabalFile args = Args.cabalFile args
+                       | otherwise                        = Args.outputCabalFile args
+
+           writeFile outFile (showGenericPackageDescription pkgDescrp')
+           exitSuccess
--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,8 @@
 
 module Main where
 
-import Distribution.PackageDescription.Parse (parsePackageDescription, ParseResult(..))
-import Distribution.PackageDescription.PrettyPrint (showGenericPackageDescription)
 import qualified CabalBounds.Args as Args
-import CabalBounds.Command (command)
-import CabalBounds.Execute (execute)
-import qualified System.IO.Strict as SIO
-import System.Exit (exitFailure, exitSuccess)
-import System.IO (hPutStrLn, stderr)
+import CabalBounds.Main (cabalBounds)
 
 main :: IO ()
-main = do
-   args <- Args.get
-   file <- SIO.readFile (Args.cabalFile args)
-   case parsePackageDescription file of
-        ParseFailed error   -> hPutStrLn stderr ("cabal-bounds: " ++ show error) >> exitFailure
-        ParseOk _ pkgDescrp -> do
-           let pkgDescrp' = execute (command args) pkgDescrp
-               outFile | null $ Args.outputCabalFile args = Args.cabalFile args
-                       | otherwise                        = Args.outputCabalFile args
-
-           writeFile outFile (showGenericPackageDescription pkgDescrp')
-           exitSuccess
+main = Args.get >>= cabalBounds
