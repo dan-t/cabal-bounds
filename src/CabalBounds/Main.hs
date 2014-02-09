1487f9cc5768232100ffbeea7eecf46518484446
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-02-09 18:47:40 +0100

    Don't call 'exit' from 'cabalBounds' function

--- src/CabalBounds/Main.hs
+++ src/CabalBounds/Main.hs
@@ -9,18 +9,18 @@ import qualified CabalBounds.Args as Args
 import CabalBounds.Command (command)
 import CabalBounds.Execute (execute)
 import qualified System.IO.Strict as SIO
-import System.Exit (exitFailure, exitSuccess)
-import System.IO (hPutStrLn, stderr)
 
-cabalBounds :: Args.Args -> IO ()
+type Error = String
+
+cabalBounds :: Args.Args -> IO (Maybe Error)
 cabalBounds args  = do
    file <- SIO.readFile (Args.cabalFile args)
    case parsePackageDescription file of
-        ParseFailed error   -> hPutStrLn stderr ("cabal-bounds: " ++ show error) >> exitFailure
+        ParseFailed error   -> return . Just $ show error
         ParseOk _ pkgDescrp -> do
            let pkgDescrp' = execute (command args) pkgDescrp
                outFile | null $ Args.outputCabalFile args = Args.cabalFile args
                        | otherwise                        = Args.outputCabalFile args
 
            writeFile outFile (showGenericPackageDescription pkgDescrp')
-           exitSuccess
+           return Nothing
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,15 @@
 
 module Main where
 
+import System.Exit (exitFailure, exitSuccess)
+import System.IO (hPutStrLn, stderr)
 import qualified CabalBounds.Args as Args
 import CabalBounds.Main (cabalBounds)
 
 main :: IO ()
-main = Args.get >>= cabalBounds
+main = do
+  args  <- Args.get
+  error <- cabalBounds args
+  case error of
+       Just err -> hPutStrLn stderr ("cabal-bounds: " ++ err) >> exitFailure
+       _        -> exitSuccess
