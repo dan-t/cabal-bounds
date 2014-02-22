1e10537aef8d31d828d5e8ad6aafae7e1eaf6a90
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-02-22 18:09:03 +0100

    Add tests for update command

--- tests/Main.hs
+++ tests/Main.hs
@@ -29,8 +29,59 @@ dropTests = testGroup "Drop Tests"
    , dropTest "DropUpperOfTest" True False [] ["some-test"] []
    ]
 
+
 updateTests :: TestTree
-updateTests = testGroup "Update Tests" []
+updateTests = testGroup "Update Tests"
+   [ updateTest "UpdateBothOfAll" False False False [] [] []
+   , updateTest "UpdateBothOfAll" True True False [] [] []
+   , updateTest "UpdateBothOfAllExes" True True False ["cabal-bounds", "other-exe"] [] []
+   , updateTest "UpdateBothOfExe" True True False ["cabal-bounds"] [] []
+   , updateTest "UpdateBothOfLibrary" True True True [] [] []
+   , updateTest "UpdateBothOfOtherExe" True True False ["other-exe"] [] []
+   , updateTest "UpdateBothOfTest" True True False [] ["some-test"] []
+   , updateTest "UpdateLowerOfAll" True False False [] [] []
+   , updateTest "UpdateLowerOfAllExes" True False False ["cabal-bounds", "other-exe"] [] []
+   , updateTest "UpdateLowerOfExe" True False False ["cabal-bounds"] [] []
+   , updateTest "UpdateLowerOfLibrary" True False True [] [] []
+   , updateTest "UpdateLowerOfOtherExe" True False False ["other-exe"] [] []
+   , updateTest "UpdateLowerOfTest" True False False [] ["some-test"] []
+   , updateTest "UpdateUpperOfAll" False True False [] [] []
+   , updateTest "UpdateUpperOfAllExes" False True False ["cabal-bounds", "other-exe"] [] []
+   , updateTest "UpdateUpperOfExe" False True False ["cabal-bounds"] [] []
+   , updateTest "UpdateUpperOfLibrary" False True True [] [] []
+   , updateTest "UpdateUpperOfOtherExe" False True False ["other-exe"] [] []
+   , updateTest "UpdateUpperOfTest" False True False [] ["some-test"] []
+   ] 
+
+
+updateTest :: String -> Bool -> Bool -> Bool -> [String] -> [String] -> [String] -> TestTree 
+updateTest testName lower upper lib exes tests benchms =
+   goldenVsFileDiff testName diff goldenFile outputFile command
+   where
+      command = do
+         let args = Args.Update { Args.lower           = lower
+                                , Args.upper           = upper
+                                , Args.library         = lib
+                                , Args.executable      = exes
+                                , Args.testSuite       = tests
+                                , Args.benchmark       = benchms
+                                , Args.cabalFile       = inputFile
+                                , Args.outputCabalFile = outputFile
+                                , Args.setupConfigFile = setupConfigFile
+                                }
+
+         error <- cabalBounds args
+         case error of
+              Just err -> hPutStrLn stderr ("cabal-bounds: " ++ err)
+              _        -> return ()
+
+
+      diff ref new = ["diff", "-u", ref, new]
+
+      goldenFile      = "tests" </> "goldenFiles" </> testName <.> "cabal"
+      outputFile      = "tests" </> "outputFiles" </> testName <.> "cabal"
+      inputFile       = "tests" </> "inputFiles"  </> "original.cabal"
+      setupConfigFile = "tests" </> "inputFiles"  </> "setup-config"
 
 
 dropTest :: String -> Bool -> Bool -> [String] -> [String] -> [String] -> TestTree
@@ -57,4 +108,4 @@ dropTest testName upper lib exes tests benchms =
 
       goldenFile = "tests" </> "goldenFiles" </> testName <.> "cabal"
       outputFile = "tests" </> "outputFiles" </> testName <.> "cabal"
-      inputFile  = "tests" </> "inputFiles"  </> testName <.> "cabal"
+      inputFile  = "tests" </> "inputFiles"  </> "original.cabal"
