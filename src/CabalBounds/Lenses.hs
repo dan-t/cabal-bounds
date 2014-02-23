9ff8aa38169238fd698c009ff7aa2d0a7a6aae35
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-02-23 15:58:46 +0100

    Get all dependencies, also the ones in the conditional trees

--- src/CabalBounds/Lenses.hs
+++ src/CabalBounds/Lenses.hs
@@ -5,6 +5,7 @@ module CabalBounds.Lenses where
 import Distribution.PackageDescription
 import Distribution.Package
 import Control.Lens
+import Data.Data.Lens
 
 makeLensesFor [ ("condLibrary"    , "condLibraryL")
               , ("condExecutables", "condExecutablesL")
@@ -23,24 +24,24 @@ makeLensesFor [ ("testName", "testNameL")
 
 makeLensesFor [ ("benchmarkName", "benchmarkNameL")
               ] ''Benchmark
- 
+
 dependenciesOfLib :: Traversal' GenericPackageDescription [Dependency]
-dependenciesOfLib = condLibraryL . _Just . condTreeConstraintsL
+dependenciesOfLib = condLibraryL . _Just . biplate
 
 dependenciesOfAllExes :: Traversal' GenericPackageDescription [Dependency]
-dependenciesOfAllExes = condExecutablesL . traversed . _2 . condTreeConstraintsL
+dependenciesOfAllExes = condExecutablesL . traversed . _2 . biplate
 
 dependenciesOfExe :: String -> Traversal' GenericPackageDescription [Dependency]
-dependenciesOfExe exe = condExecutablesL . traversed . filtered ((== exe) . fst) . _2 . condTreeConstraintsL
+dependenciesOfExe exe = condExecutablesL . traversed . filtered ((== exe) . fst) . _2 . biplate
 
 dependenciesOfAllTests :: Traversal' GenericPackageDescription [Dependency]
-dependenciesOfAllTests = condTestSuitesL . traversed . _2 . condTreeConstraintsL
+dependenciesOfAllTests = condTestSuitesL . traversed . _2 . biplate
 
 dependenciesOfTest :: String -> Traversal' GenericPackageDescription [Dependency]
-dependenciesOfTest test = condTestSuitesL . traversed . filtered ((== test) . fst) . _2 . condTreeConstraintsL
+dependenciesOfTest test = condTestSuitesL . traversed . filtered ((== test) . fst) . _2 . biplate
 
 dependenciesOfAllBenchms :: Traversal' GenericPackageDescription [Dependency]
-dependenciesOfAllBenchms = condBenchmarksL . traversed . _2 . condTreeConstraintsL
+dependenciesOfAllBenchms = condBenchmarksL . traversed . _2 . biplate
 
 dependenciesOfBenchm :: String -> Traversal' GenericPackageDescription [Dependency]
-dependenciesOfBenchm benchm = condBenchmarksL . traversed . filtered ((== benchm) . fst) . _2 . condTreeConstraintsL
+dependenciesOfBenchm benchm = condBenchmarksL . traversed . filtered ((== benchm) . fst) . _2 . biplate
