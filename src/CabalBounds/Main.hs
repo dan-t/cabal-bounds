74759301713673ecd82d35bbb64491677c840dc8
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-03-08 16:25:05 +0100

    Simplify (?) code using an EitherT transformer

--- src/CabalBounds/Main.hs
+++ src/CabalBounds/Main.hs
@@ -17,41 +17,44 @@ import qualified CabalBounds.Drop as D
 import qualified CabalBounds.Update as U
 import qualified System.IO.Strict as SIO
 import Control.Applicative ((<$>))
+import Control.Monad.Trans.Either (EitherT, runEitherT, bimapEitherT, hoistEither, left, right)
+import Control.Monad.IO.Class
 
 type Error = String
 
 cabalBounds :: A.Args -> IO (Maybe Error)
 cabalBounds args@A.Drop {} = do
-   pkgDescrp <- packageDescription $ A.cabalFile args
-   case pkgDescrp of
-        Left  error      -> return . Just $ error
-        Right pkgDescrp_ -> do
-           let pkgDescrp' = D.drop (B.boundOfDrop args) (S.sections args) (DP.dependencies args) pkgDescrp_
-           writeFile (A.outputFile args) (showGenericPackageDescription pkgDescrp')
-           return Nothing
+   leftToJust <$> (runEitherT $ do
+      pkgDescrp <- packageDescription $ A.cabalFile args
+      let pkgDescrp' = D.drop (B.boundOfDrop args) (S.sections args) (DP.dependencies args) pkgDescrp
+      liftIO $ writeFile (A.outputFile args) (showGenericPackageDescription pkgDescrp')
+      right ())
 
 cabalBounds args@A.Update {} = do
-   pkgDescrp <- packageDescription $ A.cabalFile args
-   buildInfo <- localBuildInfo $ A.setupConfigFile args
-   case (pkgDescrp, buildInfo) of
-        (Left error, _) -> return . Just $ error
-        (_, Left error) -> return . Just $ error
-        (Right pkgDescrp_, Right buildInfo_) -> do
-           let pkgDescrp' = U.update (B.boundOfUpdate args) (S.sections args) (DP.dependencies args) pkgDescrp_ buildInfo_
-           writeFile (A.outputFile args) (showGenericPackageDescription pkgDescrp')
-           return Nothing
+   leftToJust <$> (runEitherT $ do
+      pkgDescrp <- packageDescription $ A.cabalFile args
+      buildInfo <- localBuildInfo $ A.setupConfigFile args
+      let pkgDescrp' = U.update (B.boundOfUpdate args) (S.sections args) (DP.dependencies args) pkgDescrp buildInfo
+      liftIO $ writeFile (A.outputFile args) (showGenericPackageDescription pkgDescrp')
+      right ())
 
 
-packageDescription :: FilePath -> IO (Either Error GenericPackageDescription)
+packageDescription :: FilePath -> EitherT Error IO GenericPackageDescription
 packageDescription file = do
-   contents <- SIO.readFile file
+   contents <- liftIO $ SIO.readFile file
    case parsePackageDescription contents of
-        ParseFailed error   -> return . Left  $ show error
-        ParseOk _ pkgDescrp -> return . Right $ pkgDescrp
+        ParseFailed error   -> left $ show error
+        ParseOk _ pkgDescrp -> right pkgDescrp
 
 
-localBuildInfo :: FilePath -> IO (Either Error LocalBuildInfo)
-localBuildInfo file =
-   either (Left . show) Right <$> tryGetConfigStateFile file
+localBuildInfo :: FilePath -> EitherT Error IO LocalBuildInfo
+localBuildInfo file =  do
+   binfo <- liftIO $ tryGetConfigStateFile file
+   bimapEitherT show id (hoistEither binfo)
+
+
+leftToJust :: Either a b -> Maybe a
+leftToJust = either Just (const Nothing)
+
 
 deriving instance Show ConfigStateFileErrorType
