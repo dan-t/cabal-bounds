795770cfaa2dd151bad4ead67e8436bb70dc54c6
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-08-08 21:33:00 +0200

    Some reformating of code

--- lib/CabalBounds/Args.hs
+++ lib/CabalBounds/Args.hs
@@ -99,26 +99,35 @@ defaultUpdate = Update
 
 dropArgs :: Args
 dropArgs = Drop
-   { upper           = def &= explicit &= name "upper" &= name "U" &= help "Only the upper bound is dropped, otherwise both - the lower and upper - bounds are dropped."
-   , library         = def &= explicit &= name "library" &= name "l" &= help "Only the bounds of the library are modified."
-   , executable      = def &= typ "NAME" &= help "Only the bounds of the executable are modified."
-   , testSuite       = def &= typ "NAME" &= help "Only the bounds of the test suite are modified."
-   , benchmark       = def &= typ "NAME" &= help "Only the bounds of the benchmark are modified."
-   , only            = def &= explicit &= typ "DEPENDENCY" &= name "only" &= name "O" &= help "Only the bounds of the dependency are modified."
-   , ignore          = def &= explicit &= typ "DEPENDENCY" &= name "ignore" &= name "I" &= help "This dependency is ignored, not modified in any way."
-   , output = def &= explicit &= typ "FILE" &= name "output" &= name "o" &= help "Save modified cabal file to file, if empty, the cabal file is modified inplace."
-   , cabalFile       = def &= argPos 0 &= typ "CABAL-FILE"
+   { upper      = def &= explicit &= name "upper" &= name "U"
+                      &= help "Only the upper bound is dropped, otherwise both - the lower and upper - bounds are dropped."
+   , library    = def &= explicit &= name "library" &= name "l" &= help "Only the bounds of the library are modified."
+   , executable = def &= typ "NAME" &= help "Only the bounds of the executable are modified."
+   , testSuite  = def &= typ "NAME" &= help "Only the bounds of the test suite are modified."
+   , benchmark  = def &= typ "NAME" &= help "Only the bounds of the benchmark are modified."
+   , only       = def &= explicit &= typ "DEPENDENCY" &= name "only" &= name "O"
+                      &= help "Only the bounds of the dependency are modified."
+   , ignore     = def &= explicit &= typ "DEPENDENCY" &= name "ignore" &= name "I"
+                      &= help "This dependency is ignored, not modified in any way."
+   , output     = def &= explicit &= typ "FILE" &= name "output" &= name "o"
+                      &= help "Save modified cabal file to file, if empty, the cabal file is modified inplace."
+   , cabalFile  = def &= argPos 0 &= typ "CABAL-FILE"
    }
 
 
 updateArgs :: Args
 updateArgs = Update
-   { lower           = def &= explicit &= name "lower" &= name "L" &= help "Only the lower bound is updated. The same as using '--lowercomp=minor'."
-   , upper           = def &= explicit &= name "upper" &= name "U" &= help "Only the upper bound is updated. The same as using '--uppercomp=major2'."
-   , lowerComp       = def &= explicit &= name "lowercomp" &= help "Only the lower bound is updated with the specified version component. (major1, major2 or minor)"
-   , upperComp       = def &= explicit &= name "uppercomp" &= help "Only the upper bound is updated with the specified version component. (major1, major2 or minor)"
+   { lower           = def &= explicit &= name "lower" &= name "L"
+                           &= help "Only the lower bound is updated. The same as using '--lowercomp=minor'."
+   , upper           = def &= explicit &= name "upper" &= name "U"
+                           &= help "Only the upper bound is updated. The same as using '--uppercomp=major2'."
+   , lowerComp       = def &= explicit &= name "lowercomp"
+                           &= help "Only the lower bound is updated with the specified version component. (major1, major2 or minor)"
+   , upperComp       = def &= explicit &= name "uppercomp"
+                           &= help "Only the upper bound is updated with the specified version component. (major1, major2 or minor)"
    , missing         = def &= help "Only the dependencies having missing bounds are updated."
-   , haskellPlatform = def &= explicit &= typ "VERSION" &= name "haskell-platform" &= help "Update bounds by the library versions of the specified haskell platform version"
+   , haskellPlatform = def &= explicit &= typ "VERSION" &= name "haskell-platform"
+                           &= help "Update bounds by the library versions of the specified haskell platform version"
    , setupConfigFile = def &= args &= typ "SETUP-CONFIG-FILE"
    }
 
